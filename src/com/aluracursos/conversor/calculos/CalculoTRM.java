b33a0970742d8427499ecdaccf0004c2fb4b2aa8
Harold Cortes <harcor59p@gmail.com>
2024-10-22 23:32:44 -0500

    ultimos cambios

---
+++ src/com/aluracursos/conversor/calculos/CalculoTRM.java
@@ -0,0 +1,11 @@
+package com.aluracursos.conversor.calculos;
+
+public class CalculoTRM {
+    private String moneda1 ;
+    private String moneda2 ;
+    private double valor ;
+
+    public void calculoTrm(moneda1 , moneda2) {
+
+    }
+}
--- src/com/aluracursos/conversor/principal/Principal.java
+++ src/com/aluracursos/conversor/principal/Principal.java
@@ -9,7 +9,9 @@ public class Principal {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
         ConsultaTasas consulta = new ConsultaTasas();
-        System.out.println("Escriba el codigo de la moneda que quiere de referencia: ");
+        System.out.println("**********************************************************");
+        System.out.println("Bienvenido al conversor de Monedas");
+        System.out.println("Escriba el codigo de la moneda que quiere de referencia (USD - Dolar Americano) - (COP -  Peso Colombiano) - (ARS -  Peso Argentino) - (EUR - Euro) - (JPY Yen Japones): ");
         var codigoMoneda = String.valueOf(lectura.nextLine()) ;
         Moneda moneda = consulta.consultaTasa(codigoMoneda) ;
         System.out.println(moneda);
